b872c459855a2831e9cd6d01ed5d92637f8160d3
Michael Bakker <michaelbakker1@gmail.com>
2021-05-23 12:17:35 +0200

    Plugin configurations

---
+++ functions/target/debug/deps/functions-69ed9f065f0fd185.d
@@ -0,0 +1,5 @@
+c:\Users\MB\Source\RUST\The Rust Book\The-Rust-Book\functions\target\debug\deps\functions-69ed9f065f0fd185.rmeta: src\main.rs
+
+c:\Users\MB\Source\RUST\The Rust Book\The-Rust-Book\functions\target\debug\deps\functions-69ed9f065f0fd185.d: src\main.rs
+
+src\main.rs:
---
+++ variables/target/debug/deps/variables-92053ed3e4d3364d.d
@@ -0,0 +1,5 @@
+c:\Users\MB\Source\RUST\The Rust Book\The-Rust-Book\variables\target\debug\deps\variables-92053ed3e4d3364d.rmeta: src\main.rs
+
+c:\Users\MB\Source\RUST\The Rust Book\The-Rust-Book\variables\target\debug\deps\variables-92053ed3e4d3364d.d: src\main.rs
+
+src\main.rs:
